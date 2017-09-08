4e6d08ee473f3a52e1f6c8829f7a9d577cbad3aa
Abijit Suresh Geetha Ram <abijit.suresh@outlook.com>
2017-09-07 19:10:06 -0500

    Initial commit.

---
+++ app.js
@@ -0,0 +1,21 @@
+// display on console
+//console.log('My Node app!');
+
+// timeout working
+// setTimeout(function() {
+//     console.log('1 seconds have passed');
+// }, 1000);
+
+// setInterval and clearInterval
+// var time = 0;
+// var timer = setInterval(function() {
+//     time +=2;
+//     console.log(time + ' seconds have passed');
+//     if(time == 8)
+//         clearInterval(timer);
+// }, 2000);
+
+// display directory functions
+console.log(__dirname);
+console.log(__filename);
+
